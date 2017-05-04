94771ee185c9661b007adb56ea6446e883720e3c
Dennis Windsor <dennis.windsor@gmail.com>
2017-05-03 22:13:39 -0400

    Added data and script to create csv for easier processing

---
+++ create_csv.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  2 18:52:35 2017
+
+@author: dennis
+"""
+
+import os
+import csv
+
+
+train_data = "/speechdata/Training/"
+
+output = open("phone_data.csv", "w+")
+output_writer = csv.writer(output)
+
+for foldername in os.listdir(train_data):
+    for filename in os.listdir(train_data + foldername):
+        if "mfcc" in filename:
+            mfcc = open(train_data + foldername + "/" + filename)
+            phn = open(train_data + foldername + "/" + filename[:-5] + ".phn")
+            for line in phn:
+                phn_data = line.split()
+                phones.add(phn_data[2])
+                start = int(phn_data[0])
+                end = int(phn_data[1])
+                for _ in range(end-start):
+                    try:
+                        line = next(mfcc).split()
+                        line = [float(x) for x in line]
+                        output_writer.writerow([phn_data[2]] + line)
+                    except StopIteration:
+                        pass
